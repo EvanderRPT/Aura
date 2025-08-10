0e106becd640fa6cbe0665270ed6274f10236f74
EvanderRPT <evanderrampenter@gmail.com>
2025-08-10 08:14:36 +0800

    Aura Input Component

---
+++ Source/Aura/Private/Input/AuraInputComponent.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Input/AuraInputComponent.h"
+
+
+UAuraInputComponent::UAuraInputComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
+
+
+
