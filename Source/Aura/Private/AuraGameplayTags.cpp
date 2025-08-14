0c98ff01450606561b7e412971096d9359604fb9
EvanderRPT <evanderrampenter@gmail.com>
2025-08-14 08:19:47 +0800

    Set by Caller Magnitude

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -95,5 +95,11 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	FName("InputTag.4"), 
 	FString("Input Tag for 4 key Button")
 	);
-	
+
+
+	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Damage"), 
+	FString("Damage")
+	);
+
 }
