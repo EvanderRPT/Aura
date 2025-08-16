a530d694164ac524d8d739f2b8a2328ba5b24010
EvanderRPT <evanderrampenter@gmail.com>
2025-08-16 10:10:49 +0800

    Implementing Net Serialize

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "GameplayEffectTypes.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{	
+	GENERATED_BODY()
+public:
+	bool IsCriticalHit() const { return bIsBlockedHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+	void SetIsCriticalHit(bool bInIsCriticalHit ) { bIsBlockedHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+	
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FAuraGameplayEffectContext::GetScriptStruct();
+	}
+
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+
+	UFUNCTION()
+	bool bIsBlockedHit = false;
+	
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
