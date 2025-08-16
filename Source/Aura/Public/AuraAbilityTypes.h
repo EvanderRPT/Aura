83cb01c15fc785fdcb3f72954fbc4e479474092a
EvanderRPT <evanderrampenter@gmail.com>
2025-08-17 06:15:24 +0800

    fix compile issues

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -1,46 +1,53 @@
 ﻿#pragma once
+
+#include "CoreMinimal.h"
 #include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
 
 USTRUCT(BlueprintType)
 struct FAuraGameplayEffectContext : public FGameplayEffectContext
-{	
+{
 	GENERATED_BODY()
+
 public:
-	bool IsCriticalHit() const { return bIsBlockedHit; }
-	bool IsBlockedHit() const { return bIsBlockedHit; }
-	void SetIsCriticalHit(bool bInIsCriticalHit ) { bIsBlockedHit = bInIsCriticalHit; }
-	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
-	
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit()  const { return bIsBlockedHit;  }
+
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit)   { bIsBlockedHit  = bInIsBlockedHit;  }
+
 	virtual UScriptStruct* GetScriptStruct() const override
 	{
 		return StaticStruct();
 	}
 
+	// Custom net-serialize: remember to enable WithNetSerializer in traits below
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
 
 	virtual FAuraGameplayEffectContext* Duplicate() const override
 	{
 		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
 		*NewContext = *this;
+
 		if (GetHitResult())
 		{
-			// Does a deep copy of the hit result
-			NewContext->AddHitResult(*GetHitResult(), true);
+			// deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), /*bReplace=*/true);
 		}
 		return NewContext;
 	}
-	
-protected:
 
+protected:
 	UPROPERTY()
 	bool bIsBlockedHit = false;
-	
+
 	UPROPERTY()
 	bool bIsCriticalHit = false;
 };
 
+// NOTE: The base here MUST be your struct type, not FGameplayEffectContext.
 template<>
-struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : TStructOpsTypeTraitsBase2<FGameplayEffectContext>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
 {
 	enum
 	{
