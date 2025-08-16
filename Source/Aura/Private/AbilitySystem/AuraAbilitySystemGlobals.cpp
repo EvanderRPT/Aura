7e0a089f1d790e64365b1b7131c79359abb3e8ed
EvanderRPT <evanderrampenter@gmail.com>
2025-08-17 07:23:05 +0800

    Aura Ability System Globals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FGameplayEffectContext();
+}
