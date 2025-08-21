0d9c0689c4b0720358cae7a41956bf17f1689a25
EvanderRPT <evanderrampenter@gmail.com>
2025-08-22 07:13:39 +0800

    Montage and Socket Tags

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -171,18 +171,39 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 
 
 	/**
-	 * Montage
+	 * CombatSocket
 	 */
-	GameplayTags.Montage_Attack_Weapon = UGameplayTagsManager::Get().AddNativeGameplayTag(
-	FName("Montage.Attack.Weapon"), 
+	GameplayTags.CombatSocket_Attack_Weapon = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("CombatSocket.Attack.Weapon"), 
 	FString("Weapon ")
 	);
-	GameplayTags.Montage_Attack_RightHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
-	FName("Montage.Attack.RightHand"), 
+	GameplayTags.CombatSocket_Attack_RightHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("CombatSocket.Attack.RightHand"), 
 	FString("Right Hand ")
 	);
-	GameplayTags.Montage_Attack_LeftHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
-	FName("Montage.Attack.LeftHand"), 
+	GameplayTags.CombatSocket_Attack_LeftHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("CombatSocket.Attack.LeftHand"), 
 	FString("Left Hand ")
+	);
+
+
+	/**
+	 * Montage Tags
+	 */
+	GameplayTags.Montage_Attack_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Montage.Attack.1"), 
+	FString("Attack  1")
+	);
+	GameplayTags.Montage_Attack_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Montage.Attack.2"), 
+	FString("Attack  2")
+	);
+	GameplayTags.Montage_Attack_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Montage.Attack.3"), 
+	FString("Attack  3")
+	);
+	GameplayTags.Montage_Attack_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Montage.Attack.4"), 
+	FString("Attack  4")
 	);
 }
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -52,9 +52,15 @@ public:
 
 	FGameplayTag Abilities_Attack;
 
-	FGameplayTag Montage_Attack_Weapon;
-	FGameplayTag Montage_Attack_RightHand;
-	FGameplayTag Montage_Attack_LeftHand;
+	FGameplayTag CombatSocket_Attack_Weapon;
+	FGameplayTag CombatSocket_Attack_RightHand;
+	FGameplayTag CombatSocket_Attack_LeftHand;
+
+	FGameplayTag Montage_Attack_1;
+	FGameplayTag Montage_Attack_2;
+	FGameplayTag Montage_Attack_3;
+	FGameplayTag Montage_Attack_4;
+	
 	
 
 	TMap<FGameplayTag, FGameplayTag> DamageTypesToResistances;
