9562ae9edd311e4ceb685bc8c7892df1d2e01eee
EvanderRPT <evanderrampenter@gmail.com>
2025-09-13 08:56:08 +0800

    Spell Descriptions

---
+++ Source/Aura/Public/AbilitySystem/Abilities/Electrocute.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraBeamSpell.h"
+#include "Electrocute.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UElectrocute : public UAuraBeamSpell
+{
+	GENERATED_BODY()
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+};
